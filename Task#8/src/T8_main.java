1827834128ee494ea1d5553ca572c204c11d0c47
Tetiana Ustymenko <92719887+tetiana-ustymenko@users.noreply.github.com>
2021-11-01 14:21:29 +0200

    Update T8_main.java

--- Task#8/src/T8_main.java
+++ Task#8/src/T8_main.java
@@ -2,38 +2,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class T8_main {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
 
-        try (BufferedReader br = new BufferedReader(new FileReader("employee.txt"))) {
-            String s;
-            int i = -1;
-
-            while ((s = br.readLine()) != null) {
-                String[] words = s.split(" ");
-                SalariedEmployee sEm = new SalariedEmployee();
-                ContractEmployee cEmt = new ContractEmployee();
-
-                if (words.length == 3) {
-                    ++i;
-                    float temp = Float.parseFloat(words[2]);
-                    employees[i] = new SalariedEmployee(words[0], words[1], temp);
-                }
-
-                if (words.length == 4) {
-                    ++i;
-                    int temp = Integer.parseInt(words[2]);
-                    int temp2 = Integer.parseInt(words[2]);
-                    employees[i] = new ContractEmployee(words[0], words[1], temp, temp2);
-                }
-            }
-        } catch (IOException ex) {
-
-            System.out.println(ex.getMessage());
-        }
-/*
         SalariedEmployee sEm1 = new SalariedEmployee("Gabriel", "150-36-XXXX", 25600);
         SalariedEmployee sEm2 = new SalariedEmployee("Elliott", "625-99-XXXX", 30450);
         SalariedEmployee sEm3 = new SalariedEmployee("Luke", "356-63-XXXX", 40900);
@@ -41,39 +15,48 @@ public class T8_main {
         ContractEmployee cEm2 = new ContractEmployee("Matthew ", "5885022951561", 254, 35);
         Employee[] employees = new Employee[]{
                 sEm1, sEm2, sEm3, cEm1, cEm2
-        };*/
+        };
 
-        QuickSort(employees, 0, employees.length - 1);
 
-        for (Employee emp : employees) {
-            emp.output();
-        }
+        System.out.println("Unsorted list of employees: ");
+        sEm1.output();
+        sEm2.output();
+        sEm3.output();
+        cEm1.output();
+        cEm2.output();
+        System.out.println();
 
-    }
+        float[] arr = new float[5];
+        arr[0] = sEm1.calculatePay();
+        arr[1] = sEm2.calculatePay();
+        arr[2] = sEm3.calculatePay();
+        arr[3] = cEm1.calculatePay();
+        arr[4] = cEm2.calculatePay();
 
-    static int Partitioning(Employee arr[], int start, int end) {
-        float x = arr[end].calculatePay();
-        int i = start - 1;
-        for (int j = start; j < end; j++) {
-            if (arr[j].calculatePay() <= x) {
-                i++;
-                Employee temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        float tmp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+
+                }
             }
         }
-        Employee temp = arr[i + 1];
-        arr[i + 1] = arr[end];
-        arr[end] = temp;
-        return i + 1;
-    }
-
-    static void QuickSort(Employee employees[], int left, int right) {
-        int s;
-        if (left < right) {
-            s = Partitioning(employees, left, right);
-            QuickSort(employees, left, s - 1);
-            QuickSort(employees, s + 1, right);
-        }
+        System.out.println("Sorted average monthly wage: " + Arrays.toString(arr));
+        System.out.println();
+        System.out.println("Sorted list of employees: ");
+        sEm1.output2();
+        System.out.println("Average monthly wage: " + arr[0]);
+        sEm2.output2();
+        System.out.println("Average monthly wage: " + arr[1]);
+        sEm3.output2();
+        System.out.println("Average monthly wage: " + arr[2]);
+        cEm1.output2();
+        System.out.println("Average monthly wage: " + arr[3]);
+        cEm2.output2();
+        System.out.println("Average monthly wage: " + arr[4]);
     }
 }
+
